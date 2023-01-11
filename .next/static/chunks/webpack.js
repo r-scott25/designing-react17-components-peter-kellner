7f414fd8242257cc4b105d66fb70172411c24999
r-scott25 <rachelbernal2@gmail.com>
2023-01-11 13:16:36 -0600

    added _document.js file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8cfb07bb290f4e15"; }
+/******/ 		__webpack_require__.h = function() { return "4ca04ae5d87f79b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
