1ff476f8eddbc4e68b5a68668e401c439b7a1b7c
r-scott25 <rachelbernal2@gmail.com>
2023-01-11 16:35:30 -0600

    up to date

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0766feabe993678e"; }
+/******/ 		__webpack_require__.h = function() { return "610097e31e6a2bf1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -3,7 +3,6 @@ import SpeakersList from "./SpeakersList";
 import Header from "./Header";
 import SpeakersToolbar from "./SpeakersToolbar";
 
-
 function Speakers() {
   return (
     <div className="container-fluid">
